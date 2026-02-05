57002e29ddba4c41e0ed12ab7cbc40915fe878f2
punleu123 <146927040+punleu123@users.noreply.github.com>
2026-02-05 11:22:10 +0700

    Start code

---
+++ lib/services/rides_service.dart
@@ -0,0 +1,23 @@
+import '../dummy_data/dummy_data.dart';
+import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+
+  static List<Ride> filterByDeparture(Location departure) {
+    return [];
+  }
+
+  static List<Ride> filterBySeatRequested(Location departure) {
+    return [];
+  }
+
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return [];
+  }
+}
